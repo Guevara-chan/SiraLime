03f5acc53c8c236bc6cbf54de0766b6b8840bc5e
Victoria Guevara <Guevara-chan@mail.ru>
2019-07-12 22:36:05 +0300

    Add files via upload

--- src/boot.cs
+++ src/boot.cs
@@ -1,14 +1,24 @@
 using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 static class SiraLime {
 	static void Main() {
+        string title = "";
 		using (Process node = new Process()) {
 			node.StartInfo.FileName = "bin\\node.exe";
     		node.StartInfo.Arguments = "-e require('./src/node_modules/coffeescript/register.js');require('./src/main.coffee')";
     		node.StartInfo.UseShellExecute = false;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && AttachConsole(-1)) 
+                {title = Console.Title; Console.Title = ".[SiraLime]."; Console.WriteLine("");}
+            else node.StartInfo.CreateNoWindow = true;
     		node.Start();
     		node.WaitForExit();
 		}
+        Console.Title = title;
 	}
+
+    // P/Invoke declarations
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern bool AttachConsole(int dwProcessId);
 }
